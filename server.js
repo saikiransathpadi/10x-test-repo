6b4df005aa54025fa182a53d1761879f41a00410
SaiKiranS <sathpadisaikiran@gmail.com>
2021-01-12 15:25:59 +0530

    added corresponding comments to the files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require('http');  //inporting the module http
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('Hello, World!');  //to display on the webpage
 }
 
-const server = http.createServer(requestListener);
+const server = http.createServer(requestListener);   //creating the server
 server.listen(8080);
 
 //creating a server
